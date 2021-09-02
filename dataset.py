e05d49ff027c1b119503ef992570949e6eb0e921
moonkyung <larcane@naver.com>
2021-09-02 08:03:34 +0000

    1. added pseudo dataset class 2. changed default train.csv path 3. changed validation resize size

--- dataset.py
+++ dataset.py
@@ -20,7 +20,7 @@ def seed_everything(seed):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = True
-seed_everything(1010)
+seed_everything(42)
 
 
 class MaskAugmentation:
@@ -36,19 +36,19 @@ class MaskAugmentation:
             #                 albumentations.pytorch.ToTensorV2(),
             #                ]),
             'train' : albumentations.Compose([
-                            albumentations.Resize(256,256),
+                            albumentations.Resize(image_size[0],image_size[1]),
                             albumentations.CenterCrop(224,224),
                             albumentations.ColorJitter(),
                             albumentations.Normalize(),
                             albumentations.GaussNoise(var_limit=(0.001,0.01)),
-                            albumentations.CoarseDropout(max_height=15,max_width=15),
+                            albumentations.CoarseDropout(max_height=25,max_width=25),
                             albumentations.RandomBrightnessContrast(),
                             albumentations.Rotate(15),
                             albumentations.HorizontalFlip(),
                             albumentations.pytorch.ToTensorV2()]),
 
             'validation' : albumentations.Compose([
-                            albumentations.Resize(image_size[0],image_size[1]),
+                            albumentations.Resize(224,224),
                             albumentations.Normalize(),
                             albumentations.pytorch.ToTensorV2()]),
                             
@@ -64,15 +64,14 @@ class MaskAugmentation:
 
 class MaskDataSet(torch.utils.data.Dataset):
   
-    def __init__(self,train_csv='/opt/ml/input/data/allconcat.csv',multi_label=False,transform=None):
-        self.train_csv=pathlib.Path(train_csv)
+    def __init__(self,train_csv='/opt/ml/input/data/train/new_standard.csv',multi_label=False,transform=None):
         self.transform=transform
         self.multi_label=multi_label
 
-        self.image_paths, self.label_classes, self.labels = self.read_csv_file(self.train_csv)
+        self.image_paths, self.label_classes, self.labels = self.read_csv_file(train_csv)
         
     def __len__(self):
-        return len(self.file_paths)    
+        return len(self.image_paths)    
     
     def __getitem__(self,idx):
         
@@ -81,7 +80,6 @@ class MaskDataSet(torch.utils.data.Dataset):
         # image data 불러오기
         image = np.array(Image.open(self.image_paths[idx]))
         if self.transform:
-            print('transformed..')
             image = self.transform(image=image)['image']
         
         if self.multi_label:
@@ -99,6 +97,47 @@ class MaskDataSet(torch.utils.data.Dataset):
         data_pd = pd.read_csv(train_dir,encoding='utf-8')
 
         return data_pd['path'], data_pd['label'], list(zip(data_pd['gender'],data_pd['age'],data_pd['mask']))
+
+
+class PseudoMaskDataSet(torch.utils.data.Dataset):
+  
+    def __init__(self,train_csv='/opt/ml/input/data/train/new_standard.csv',
+    pseudo_csv='/opt/ml/input/data/train/pseudo.csv'
+    ,multi_label=False,transform=None):
+        assert multi_label==False, "multi label is not used pseudo labeling"
+        self.transform=transform
+        self.image_paths, self.label_classes = self.read_csv_file(train_csv,pseudo_csv)
+        
+    def __len__(self):
+        return len(self.image_paths)    
+    
+    def __getitem__(self,idx):
+        
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        # image data 불러오기
+        image = np.array(Image.open(self.image_paths[idx]))
+        if self.transform:
+            image = self.transform(image=image)['image']
+    
+        y = np.array(self.label_classes[idx])
+        
+        return image,y
+        
+    def set_transform(self,transform):
+        self.transform=transform
+    
+    def read_csv_file(self,train_csv,pseudo_csv):
+        ''' Return file path using directory path in csv_file  '''
+        data_pd = pd.read_csv(train_csv,encoding='utf-8')
+        pseudo_pd = pd.read_csv(pseudo_csv,encoding='utf-8')
+        
+        path = pd.concat([pseudo_pd['ImageID'].apply(lambda x: os.path.join('/opt/ml/input/data/eval/images',x)),data_pd['path']]).reset_index(drop=True)
+        label = pd.concat([pseudo_pd['ans'],data_pd['label']]).reset_index(drop=True)
+        
+        return path, label
+                 
+                 
 
 class TestDataset(torch.utils.data.Dataset):
     def __init__(self,image_paths,transform=albumentations.Resize(256,256),pseudo_label=None,pseudo_label_path=None):
